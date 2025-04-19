fc34156f4cea0ef76ed4cf2032c6031ce8509d07
Preyash-tryin-to-code <preyashyadav29@gmail.com>
2025-04-19 04:35:38 -0700

    [UI]: Fix Font for Mobile Devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,21 @@ import { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+    <head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&family=Bangers&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Navbar />
         <main>{children}</main>
